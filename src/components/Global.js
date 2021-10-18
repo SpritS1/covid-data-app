54733e09901e052d0604414b8906645872047667
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2021-10-18 17:39:56 +0200

    Added and styled global data

--- src/components/Global.js
+++ src/components/Global.js
@@ -3,28 +3,30 @@ import './Global.scss';
 const Global = () => {
     return ( 
         <div className="global">
+            <h2 className="global__header">Global data</h2>
+
             <div className="global__data">
                 <div className="global__data-left">
-                    <span className="global__data-name">Today's cases</span>
-                    <p className="global__data-value">21,391</p>
+                    <span className="global__data-name">Today cases</span>
+                    <p className="global__data-value">21,391 <span className='global__data-new-value plus'>+2412</span></p>
                 </div>
                 <div className="global__data-icon-box">
-                    <i className='global__data-icon fas fa-star-of-life'></i>
+                    <i className='global__data-icon fas fa-virus'></i>
                 </div>
             </div>
             <div className="global__data">
                 <div className="global__data-left">
-                    <span className="global__data-name">Today's cases</span>
-                    <p className="global__data-value">21,391</p>
+                    <span className="global__data-name">Today Deaths</span>
+                    <p className="global__data-value">594 <span className='global__data-new-value minus'>-10</span></p>
                 </div>
                 <div className="global__data-icon-box">
-                    <i className='global__data-icon fas fa-star-of-life'></i>
+                    <i className='global__data-icon fas fa-cross'></i>
                 </div>
             </div>
             <div className="global__data">
                 <div className="global__data-left">
-                    <span className="global__data-name">Today's cases</span>
-                    <p className="global__data-value">21,391</p>
+                    <span className="global__data-name">Today Recovery</span>
+                    <p className="global__data-value">15,343 <span className='global__data-new-value plus'>+1231</span></p>
                 </div>
                 <div className="global__data-icon-box">
                     <i className='global__data-icon fas fa-star-of-life'></i>
