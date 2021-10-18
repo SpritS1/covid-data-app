2e9af9101c3f48ad66cb8514658efbc618f3e674
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2021-10-18 15:41:54 +0200

    Initial commit

---
+++ src/components/Global.js
@@ -0,0 +1,37 @@
+import './Global.scss';
+
+const Global = () => {
+    return ( 
+        <div className="global">
+            <div className="global__data">
+                <div className="global__data-left">
+                    <span className="global__data-name">Today's cases</span>
+                    <p className="global__data-value">21,391</p>
+                </div>
+                <div className="global__data-icon-box">
+                    <i className='global__data-icon fas fa-star-of-life'></i>
+                </div>
+            </div>
+            <div className="global__data">
+                <div className="global__data-left">
+                    <span className="global__data-name">Today's cases</span>
+                    <p className="global__data-value">21,391</p>
+                </div>
+                <div className="global__data-icon-box">
+                    <i className='global__data-icon fas fa-star-of-life'></i>
+                </div>
+            </div>
+            <div className="global__data">
+                <div className="global__data-left">
+                    <span className="global__data-name">Today's cases</span>
+                    <p className="global__data-value">21,391</p>
+                </div>
+                <div className="global__data-icon-box">
+                    <i className='global__data-icon fas fa-star-of-life'></i>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Global;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import './Home.scss';
+import Global from './Global';
+
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h1 className="home__header">COVID-19 Data</h1>
+            <Global/>
+        </div>
+     );
+}
+ 
+export default Home;
