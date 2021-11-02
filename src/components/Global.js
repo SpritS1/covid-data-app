183e788dcf913ca5ca440b4b22baffdb85fcc122
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2021-11-02 16:26:58 +0100

    Updated Global.js component. Global cases and deaths are calculated from countriesData.

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,6 +1,25 @@
+import { useMemo } from 'react/cjs/react.development';
 import './Global.scss';
 
-const Global = ({ globalData }) => {
+const Global = ({ countriesData }) => {
+    const globalData = useMemo(() => {
+        const globalData = {
+            confirmed: 0,
+            confirmed_daily: 0,
+            deaths: 0,
+            deaths_daily: 0
+        };
+        console.log(countriesData)
+        for (const data of countriesData) {
+            globalData.confirmed += data.confirmed;
+            globalData.confirmed_daily += data.confirmed_daily;
+            globalData.deaths += data.deaths;
+            globalData.deaths_daily += data.deaths_daily;
+        }
+
+        return globalData;
+    }, [countriesData])
+    
     return ( 
         <div className="global">
             <h2 className="global__header">Global Statistics</h2>
@@ -8,7 +27,7 @@ const Global = ({ globalData }) => {
             <div className="global__data">
                 <div className="global__data-left">
                     <span className="global__data-name">Today cases</span>
-                    <p className="global__data-value">{globalData && globalData.NewConfirmed.toLocaleString()} <span className='global__data-new-value plus'>+2412</span></p>
+                    <p className="global__data-value">{globalData && globalData.confirmed_daily.toLocaleString()} <span className='global__data-new-value plus'>+2412</span></p>
                 </div>
                 <div className="global__data-icon-box">
                     <i className='global__data-icon fas fa-virus'></i>
@@ -17,13 +36,13 @@ const Global = ({ globalData }) => {
             <div className="global__data">
                 <div className="global__data-left">
                     <span className="global__data-name">Today Deaths</span>
-                    <p className="global__data-value">{globalData && globalData.NewDeaths.toLocaleString()}  <span className='global__data-new-value minus'>-10</span></p>
+                    <p className="global__data-value">{globalData && globalData.deaths_daily.toLocaleString()}  <span className='global__data-new-value minus'>-10</span></p>
                 </div>
                 <div className="global__data-icon-box">
                     <i className='global__data-icon fas fa-cross'></i>
                 </div>
             </div>
-            <div className="global__data">
+            {/* <div className="global__data">
                 <div className="global__data-left">
                     <span className="global__data-name">Today Recovery</span>
                     <p className="global__data-value">{globalData && globalData.NewRecovered.toLocaleString()}  <span className='global__data-new-value plus'>+1231</span></p>
@@ -31,7 +50,7 @@ const Global = ({ globalData }) => {
                 <div className="global__data-icon-box">
                     <i className='global__data-icon fas fa-star-of-life'></i>
                 </div>
-            </div>
+            </div> */}
         </div>
      );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,13 @@ import Map from './Map/Map';
 import useFetch from '../hooks/useFetch';
 
 const Home = () => {
-    const url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/countries_summary?&min_date=2021-10-29&max_date=2021-10-29';
+    const url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/countries_summary?&min_date=2021-11-01&max_date=2021-11-01';
     const {response: countriesData, error} = useFetch(url, {});
 
     return ( 
         <div className="home">
             <Header></Header>
-            {/* {countriesData && <Global globalData={countriesData}/>} */}
+            {countriesData && <Global countriesData={countriesData}/>}
             {countriesData && <Map countriesData={countriesData} />}
             {countriesData && <CountriesTable countriesData={countriesData}/>}
         </div>
