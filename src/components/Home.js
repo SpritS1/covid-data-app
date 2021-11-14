a961d411cf42efd70eb44952637fb0bb6c534a0f
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2021-11-14 14:42:46 +0100

    Added ChartButton component used to switching chartMode to 'cases' or 'deaths'

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,8 +22,7 @@ const Home = () => {
             {countriesData && <Global countriesData={countriesData}/>}
             {countriesData && <CountriesTable countriesData={countriesData}/>}
             {countriesData && <Map countriesData={countriesData} />}
-            {countriesData && <Chart className={'cases'} chartColor={'33, 212, 253'} dataName={'confirmed_daily'} label={'New Cases'} date={date}/>}
-            {/* {countriesData && <Chart className={'deaths'} chartColor={'51, 51, 51'} dataName={'deaths_daily'} label={'New Deaths'} date={date}/>} */}
+            {countriesData && <Chart date={date}/>}
         </div>
      );
 }
