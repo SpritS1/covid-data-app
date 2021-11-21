43206346c8b5e360ba555a6559da2bbbb59f8c10
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2021-11-21 22:39:47 +0100

    Updated CountryChange and Searchbox Components. CountryChange filters countries list by a value entered in the Searchbox component

--- src/components/Header/CountryChange.js
+++ src/components/Header/CountryChange.js
@@ -1,70 +1,101 @@
-import './CountryChange.scss';
-import SearchBox from '../SearchBox';
-import { useMemo } from 'react';
+import "./CountryChange.scss";
+import SearchBox from "../SearchBox";
+import { useEffect, useMemo, useState } from "react";
 
-const CountryChange = ({ setIsActive, countriesData, selectedCountry, setSelectedCountry }) => {
-    const countriesList = useMemo(() => {
-        const array = [
-            {
-                countryName: 'Global',
-                iso2: null
-            }
-        ];
+const CountryChange = ({
+   setIsActive,
+   countriesData,
+   selectedCountry,
+   setSelectedCountry,
+}) => {
+   // Converts countriesData to array of objects with countryName and iso2
+   const countriesList = useMemo(() => {
+      const array = [
+         {
+            countryName: "Global",
+            iso2: null,
+         },
+      ];
 
-        countriesData.map((country) => {
-            if (country.country && country.country_iso2s) {
-                array.push({
-                    countryName: country.country,
-                    iso2: country.country_iso2s[0]
-                })
-            } 
-        })
+      countriesData.map((country) => {
+         if (country.country && country.country_iso2s) {
+            array.push({
+               countryName: country.country,
+               iso2: country.country_iso2s[0],
+            });
+         }
+      });
 
-        return array;
-    })
+      return array;
+   });
 
-    const handleClick = (country) => {
-        setSelectedCountry(country);
-        setIsActive(false);
-    }
+   const [searchedCountriesList, setSearchedCountriesList] =
+      useState(countriesList);
+   const [searchedValue, setSearchedValue] = useState("");
 
-    return ( 
-        <div className="country-change">
-            <div className="country-change__header">
-                <h3 className="country-change__title">Select country</h3>
-                <i className="fas fa-times country-change__close" onClick={() => setIsActive(false)}/>
-            </div>
-            <SearchBox />
-            <ul className="country-change__countries-list">
-                {
-                    countriesList.map(({countryName, iso2}) => {
-                        if (iso2 || countryName === 'Global') {
-                            return <li className="country-change__countries-list-item" onClick={() => handleClick({countryName, iso2})} key={countryName}>
-                                        {iso2 && (
-                                            <img
-                                                className='country-change__country-flag'
-                                                src={`https://flagcdn.com/24x18/${iso2.toLowerCase()}.png`}
-                                                alt={countryName + " flag"}
-                                            ></img>                                        
-                                        )}
-                                        {countryName === 'Global' && (
-                                            <i className="fas fa-globe country-change__country-flag"></i>
-                                        )}
-                                        <span className="country-name">{countryName}</span>
-                                    </li>                            
-                        }
-                    })
-                }
-                {/* {countriesData.map((country) => {
-                    if (country.country && country.country_iso2s) {
-                        return <li key={country.country}>{country.country}</li>
-                    } else {
-                        return null;
-                    }
-                })} */}
-            </ul>
-        </div>
-     );
-}
- 
+   const searchCountry = (searchedValue, countriesList) => {
+      const searchedList = countriesList.filter((country) => {
+         if (
+            country.countryName
+               .toLowerCase()
+               .includes(searchedValue.toLowerCase())
+         ) {
+            return country;
+         }
+      });
+
+      setSearchedCountriesList(searchedList);
+   };
+
+   useEffect(() => {
+      searchCountry(searchedValue, countriesList);
+   }, [searchedValue]);
+
+   const handleClick = (country) => {
+      setSelectedCountry(country);
+      setIsActive(false);
+   };
+
+   return (
+      <div className="country-change">
+         <div className="country-change__header">
+            <h3 className="country-change__title">Select country</h3>
+            <i
+               className="fas fa-times country-change__close"
+               onClick={() => setIsActive(false)}
+            />
+         </div>
+         <SearchBox
+            searchedValue={searchedValue}
+            setSearchedValue={setSearchedValue}
+         />
+         <ul className="country-change__countries-list">
+            {searchedCountriesList.map(({ countryName, iso2 }) => {
+               if (iso2 || countryName === "Global") {
+                  return (
+                     <li
+                        className="country-change__countries-list-item"
+                        onClick={() => handleClick({ countryName, iso2 })}
+                        key={countryName}
+                     >
+                        {iso2 && (
+                           <img
+                              className="country-change__country-flag"
+                              src={`https://flagcdn.com/24x18/${iso2.toLowerCase()}.png`}
+                              alt={countryName + " flag"}
+                           ></img>
+                        )}
+                        {countryName === "Global" && (
+                           <i className="fas fa-globe country-change__country-flag"></i>
+                        )}
+                        <span className="country-name">{countryName}</span>
+                     </li>
+                  );
+               }
+            })}
+         </ul>
+      </div>
+   );
+};
+
 export default CountryChange;
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,9 +1,14 @@
 import './SearchBox.scss';
 
-const SearchBox = () => {
+const SearchBox = ({ searchedValue, setSearchedValue }) => {
     return ( 
         <div className="search-box">
-            <input type="text" placeholder='Search' className="search-box__input" />
+            <input 
+                type="text" 
+                value={searchedValue}
+                onChange={(e) => setSearchedValue(e.target.value)}
+                placeholder='Search' 
+                className="search-box__input" />
             <i className="fas fa-search search-box__icon "></i>
         </div>
      );
